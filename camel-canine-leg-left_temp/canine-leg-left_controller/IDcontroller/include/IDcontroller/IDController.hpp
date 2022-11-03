6f7b5fe07a11685e73af4503664c0c8b4f9a236f
Jaehoon-An <dkswogns46@gmail.com>
2022-11-03 23:07:17 +0900

    Added inverse dynamics controller in the controllers

---
+++ camel-canine-leg-left_temp/canine-leg-left_controller/IDcontroller/include/IDcontroller/IDController.hpp
@@ -0,0 +1,42 @@
+//
+// Created by jh on 22. 11. 3.
+//
+
+#ifndef RAISIM_IDCONTROLLER_HPP
+#define RAISIM_IDCONTROLLER_HPP
+
+#include <Eigen/Eigen>
+#include <canine-leg-left_util/SharedMemory.hpp>
+#include <canine-leg-left_util/RobotDescription.hpp>
+#include <camel-tools/QuinticTrajectoryGenerator.hpp>
+
+class IDController
+{
+public:
+    IDController();
+
+    void DoControl();
+    void InitQuinticTrajectory();
+    void SetControlInput();
+
+private:
+    void setQuinticTrajectory();
+    void computeControlInput();
+
+private:
+    QuinticTrajectoryGenerator mQuinticTrajectoryGen;
+    Eigen::MatrixXd mJacobian = Eigen::MatrixXd(2, 2);
+    Eigen::VectorXd mTorque = Eigen::VectorXd(MOTOR_NUM);
+    Eigen::VectorXd mForce = Eigen::VectorXd(2);
+    double mDesiredPosition;
+    double mDesiredVelocity;
+    double mDesiredAcceleration;
+    double Kp;
+    double Kd;
+    double mTorqueLimit[MOTOR_NUM];
+    int mSign;
+
+};
+
+
+#endif //RAISIM_IDCONTROLLER_HPP

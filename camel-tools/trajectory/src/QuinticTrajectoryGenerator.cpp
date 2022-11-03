7d160f34fdb6f63dc0faec6ed9c74aa0e55a9f64
Jaehoon-An <dkswogns46@gmail.com>
2022-11-03 21:53:02 +0900

    Added time over barrier in get trajectory functions

--- camel-tools/trajectory/src/QuinticTrajectoryGenerator.cpp
+++ camel-tools/trajectory/src/QuinticTrajectoryGenerator.cpp
@@ -12,18 +12,21 @@ void QuinticTrajectoryGenerator::calculateCoefficient() {
 }
 
 double QuinticTrajectoryGenerator::getPositionTrajectory(double currentTime) {
+    if(currentTime>mReferenceTime + mTimeDuration) currentTime = mReferenceTime + mTimeDuration;
     double normalizedTime = (currentTime - mReferenceTime) / mTimeDuration;
     return mCoefficient(0,0) * pow(normalizedTime, 5.0) + mCoefficient(1,0) * pow(normalizedTime, 4.0) + mCoefficient(2,0) * pow(normalizedTime, 3.0)
             + mCoefficient(3, 0) * pow(normalizedTime, 2.0) + mCoefficient(4, 0) * normalizedTime + mCoefficient(5,0);
 }
 
 double QuinticTrajectoryGenerator::getVelocityTrajectory(double currentTime) {
+    if(currentTime>mReferenceTime + mTimeDuration) currentTime = mReferenceTime + mTimeDuration;
     double normalizedTime = (currentTime - mReferenceTime) / mTimeDuration;
     return (5.0 *mCoefficient(0,0) * pow(normalizedTime, 4.0) + 4.0 * mCoefficient(1,0) * pow(normalizedTime, 3.0) + 3.0 * mCoefficient(2,0) * pow(normalizedTime, 2.0)
             + 2.0 * mCoefficient(3,0) * normalizedTime + mCoefficient(4,0)) / mTimeDuration;
 }
 
 double QuinticTrajectoryGenerator::getAccelerationTrajectory(double currentTime) {
+    if(currentTime>mReferenceTime + mTimeDuration) currentTime = mReferenceTime + mTimeDuration;
     double normalizedTime = (currentTime - mReferenceTime) / mTimeDuration;
     return (20.0 *mCoefficient(0,0) * pow(normalizedTime, 3.0) + 12.0 * mCoefficient(1,0) * pow(normalizedTime, 2.0) + 6.0 * mCoefficient(2,0) * normalizedTime
             + 2.0 * mCoefficient(3,0)) / pow(mTimeDuration, 2.0);
